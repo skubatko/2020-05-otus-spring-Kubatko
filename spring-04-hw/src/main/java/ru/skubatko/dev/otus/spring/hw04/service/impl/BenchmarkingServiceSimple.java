03cd6b213c352a24f8079fc048a9b9f1b5b37d09
Sergey Kubatko <skubatko@gmail.com>
2020-06-14 20:23:10 +0300

    spring-04-hw / async benchmarking refactored

---
+++ spring-04-hw/src/main/java/ru/skubatko/dev/otus/spring/hw04/service/impl/BenchmarkingServiceSimple.java
@@ -0,0 +1,19 @@
+package ru.skubatko.dev.otus.spring.hw04.service.impl;
+
+import ru.skubatko.dev.otus.spring.hw04.service.BenchmarkingService;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StopWatch;
+
+@Slf4j
+@Service
+public class BenchmarkingServiceSimple implements BenchmarkingService {
+
+    @Override
+    @Async
+    public void report(StopWatch clock) {
+        log.debug(clock.prettyPrint());
+    }
+}
