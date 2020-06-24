3e787359f9addc866fb8966ea52daaab5ddd672b
Sergey Kubatko <skubatko@gmail.com>
2020-06-24 11:30:49 +0300

    spring-07-hw / initial setup

--- README.md
+++ README.md
@@ -12,3 +12,4 @@ https://otus.ru/lessons/javaspring/?int_source=courses_catalog&int_term=programm
 * spring-02-hw - домашнее задание к уроку 2 и 3
 * spring-04-hw - домашнее задание к уроку 3 и 4
 * spring-05-hw - домашнее задание к уроку 5
+* spring-07-hw - домашнее задание к уроку 7
--- spring-07-hw/src/main/java/ru/skubatko/dev/otus/spring/hw07/App.java
+++ spring-07-hw/src/main/java/ru/skubatko/dev/otus/spring/hw07/App.java
@@ -3,12 +3,13 @@
  */
 package ru.skubatko.dev.otus.spring.hw07;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        SpringApplication.run(App.class, args);
     }
 }
