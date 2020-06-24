f66706099e3c7659d625d49fb00441d076667dd0
Sergey Kubatko <skubatko@gmail.com>
2020-06-24 23:19:55 +0300

    spring-07-hw / AuthorShellCommands updated

---
+++ spring-07-hw/src/main/java/ru/skubatko/dev/otus/spring/hw07/shell/AuthorShellCommands.java
@@ -0,0 +1,86 @@
+package ru.skubatko.dev.otus.spring.hw07.shell;
+
+import ru.skubatko.dev.otus.spring.hw07.domain.Author;
+import ru.skubatko.dev.otus.spring.hw07.service.AuthorService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.shell.Availability;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellMethodAvailability;
+import org.springframework.shell.standard.ShellOption;
+
+import java.util.stream.Collectors;
+
+@ShellComponent
+@RequiredArgsConstructor
+@SuppressWarnings("unused")
+public class AuthorShellCommands {
+
+    private final AuthorService authorService;
+    private final LoginShellCommands loginShellCommands;
+
+    @ShellMethod(value = "Find author by id", key = {"fa", "findAuthorById"})
+    @ShellMethodAvailability(value = "loggedIn")
+    public String findAuthorById(@ShellOption(defaultValue = "0") String id) {
+        Author author = authorService.findById(Long.parseLong(id));
+        if (author == null) {
+            return String.format("Author with id = %s not found", id);
+        }
+        return String.format("Author: %s", author.getName());
+    }
+
+    @ShellMethod(value = "Find all authors in the library", key = {"faa", "findAllAuthors"})
+    @ShellMethodAvailability(value = "loggedIn")
+    public String findAllAuthors() {
+        return String.format("Available authors: %s",
+                authorService.findAll().stream().map(Author::getName).collect(Collectors.joining(", ")));
+    }
+
+    @ShellMethod(value = "Add author to the library", key = {"aa", "addAuthor"})
+    @ShellMethodAvailability(value = "loggedIn")
+    public String addAuthor(@ShellOption(defaultValue = "0") String id,
+                            @ShellOption(defaultValue = "unnamed") String name) {
+        int response = authorService.insert(new Author(Long.parseLong(id), name));
+        if (response != 1) {
+            return "Author cannot be added";
+        }
+
+        return String.format("Author %s added successfully", name);
+    }
+
+    @ShellMethod(value = "Update author to the library", key = {"ua", "updateAuthor"})
+    @ShellMethodAvailability(value = "loggedIn")
+    public String updateAuthor(@ShellOption(defaultValue = "0") String id,
+                               @ShellOption(defaultValue = "unnamed") String name) {
+        int response = authorService.update(new Author(Long.parseLong(id), name));
+        if (response != 1) {
+            return "Author cannot be updated";
+        }
+
+        return String.format("Author %s updated successfully", name);
+    }
+
+    @ShellMethod(value = "Delete author by id", key = {"da", "deleteAuthorById"})
+    @ShellMethodAvailability(value = "loggedIn")
+    public String deleteAuthorById(@ShellOption(defaultValue = "0") String idString) {
+        long id = Long.parseLong(idString);
+        String name = authorService.findById(id).getName();
+        int response = authorService.deleteById(id);
+        if (response != 1) {
+            return "Author cannot be deleted";
+        }
+
+        return String.format("Author %s deleted successfully", name);
+    }
+
+    @ShellMethod(value = "Get number of authors in the library", key = {"ac", "getAuthorsCount"})
+    @ShellMethodAvailability(value = "loggedIn")
+    public String getAuthorsCount() {
+        return String.format("In the library now %d author(s)", authorService.count());
+    }
+
+    private Availability loggedIn() {
+        return loginShellCommands.loggedIn();
+    }
+}
---
+++ spring-07-hw/src/main/java/ru/skubatko/dev/otus/spring/hw07/shell/LoginShellCommands.java
@@ -0,0 +1,44 @@
+package ru.skubatko.dev.otus.spring.hw07.shell;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.shell.Availability;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellMethodAvailability;
+import org.springframework.shell.standard.ShellOption;
+
+@ShellComponent
+@RequiredArgsConstructor
+@SuppressWarnings("unused")
+public class LoginShellCommands {
+
+    private String userName;
+
+    @ShellMethod(value = "Login command", key = {"l", "login"})
+    @ShellMethodAvailability(value = "notLoggedIn")
+    public String login(@ShellOption(defaultValue = "unnamed") String userName) {
+        this.userName = userName;
+        return String.format("Welcome to library, %s!", userName);
+    }
+
+    @ShellMethod(value = "Logout command", key = {"bye", "logout"})
+    @ShellMethodAvailability(value = "loggedIn")
+    public String logout() {
+        String userName = this.userName;
+        this.userName = StringUtils.EMPTY;
+        return String.format("Goodbye, %s", userName);
+    }
+
+    public Availability loggedIn() {
+        return StringUtils.isBlank(userName)
+               ? Availability.unavailable("login required")
+               : Availability.available();
+    }
+
+    private Availability notLoggedIn() {
+        return StringUtils.isBlank(userName)
+               ? Availability.available()
+               : Availability.unavailable("logout required");
+    }
+}
