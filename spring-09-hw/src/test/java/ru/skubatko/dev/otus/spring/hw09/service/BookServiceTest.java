4b03ddb8077a01ede90f7fd9b22a6c519abc27ba
Sergey Kubatko <skubatko@gmail.com>
2020-07-05 20:05:53 +0300

    spring-09-hw / устранены замечания code review

--- spring-09-hw/src/test/java/ru/skubatko/dev/otus/spring/hw09/service/BookServiceTest.java
+++ spring-09-hw/src/test/java/ru/skubatko/dev/otus/spring/hw09/service/BookServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import ru.skubatko.dev.otus.spring.hw09.domain.Author;
 import ru.skubatko.dev.otus.spring.hw09.domain.Book;
+import ru.skubatko.dev.otus.spring.hw09.domain.BookComment;
 import ru.skubatko.dev.otus.spring.hw09.domain.Genre;
 
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @DisplayName("Сервис для работы с книгами должен")
 @Transactional
@@ -28,11 +30,25 @@ class BookServiceTest {
     void shouldFindExpectedBookById() {
         Author author = new Author(2, "testAuthor2");
         Genre genre = new Genre(3, "testGenre3");
-        Book expected = new Book(2, "testBook2", author, genre, Collections.emptyList());
+        Book expected = new Book(2, "testBook2", author, genre, null);
 
         Book actual = service.findById(2);
 
         assertThat(actual).isEqualToComparingOnlyGivenFields(expected, "id", "name", "author", "genre");
+    }
+
+    @DisplayName("находить ожидаемую книгу с комментариями по её id")
+    @Test
+    void shouldFindExpectedBookByIdWithComments() {
+        Author author = new Author(2, "testAuthor2");
+        Genre genre = new Genre(3, "testGenre3");
+        Book expected = new Book(2, "testBook2", author, genre, null);
+
+        Book actual = service.findByIdWithComments(2);
+
+        assertThat(actual).isEqualToIgnoringGivenFields(expected, "bookComments");
+        assertThat(actual.getBookComments()).hasSize(1);
+        assertThat(actual.getBookComments().get(0).getContent()).isEqualTo("testBookComment2");
     }
 
     @DisplayName("находить ожидаемую книгу по её имени")
@@ -42,18 +58,55 @@ class BookServiceTest {
         Genre genre = new Genre(3, "testGenre3");
         String name = "testBook2";
         Book expected = new Book(2, name, author, genre, Collections.emptyList());
+
         Book actual = service.findByName(name);
+
         assertThat(actual).isEqualToComparingOnlyGivenFields(expected, "id", "name", "author", "genre");
+    }
+
+    @DisplayName("находить ожидаемую книгу с комментариями по её имени")
+    @Test
+    void shouldFindExpectedBookByNameWithComments() {
+        Author author = new Author(2, "testAuthor2");
+        Genre genre = new Genre(3, "testGenre3");
+        String name = "testBook2";
+        Book expected = new Book(2, name, author, genre, null);
+
+        Book actual = service.findByNameWithComments(name);
+
+        assertThat(actual).isEqualToIgnoringGivenFields(expected, "bookComments");
+        assertThat(actual.getBookComments()).hasSize(1);
+        assertThat(actual.getBookComments().get(0).getContent()).isEqualTo("testBookComment2");
     }
 
     @DisplayName("находить все книги")
     @Test
     void shouldFindAllBooks() {
         List<Book> books = service.findAll();
+
         assertThat(books)
                 .hasSize(6)
                 .extracting("name")
                 .containsOnlyOnce("testBook1", "testBook2", "testBook3", "testBook4", "testBook5", "testBook6");
+    }
+
+    @DisplayName("находить все книги с комментариями")
+    @Test
+    void shouldFindAllBooksWithComments() {
+        List<Book> books = service.findAllWithComments();
+
+        assertThat(books)
+                .hasSize(6)
+                .extracting("name")
+                .containsOnlyOnce("testBook1", "testBook2", "testBook3", "testBook4", "testBook5", "testBook6");
+
+        List<BookComment> comments =
+                books.stream().flatMap(book -> book.getBookComments().stream()).collect(Collectors.toList());
+        assertThat(comments)
+                .hasSize(6)
+                .extracting("content")
+                .containsOnlyOnce("testBookComment1", "testBookComment2", "testBookComment3",
+                        "testBookComment4", "testBookComment5", "testBookComment6");
     }
 
     @DisplayName("добавлять книгу")
