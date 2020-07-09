f6d07306b6a2a8a9989bf71beaeca79651a9667b
Sergey Kubatko <skubatko@gmail.com>
2020-07-09 13:18:07 +0300

    spring-11-hw / H2ConsoleConfig updated

--- spring-11-hw/src/main/java/ru/skubatko/dev/otus/spring/hw11/config/H2ConsoleConfig.java
+++ spring-11-hw/src/main/java/ru/skubatko/dev/otus/spring/hw11/config/H2ConsoleConfig.java
@@ -3,10 +3,12 @@ package ru.skubatko.dev.otus.spring.hw11.config;
 import org.h2.tools.Console;
 import org.springframework.context.annotation.Configuration;
 
+import java.sql.SQLException;
+
 @Configuration
 public class H2ConsoleConfig {
 
-    public H2ConsoleConfig(AppProps props) throws Exception {
+    public H2ConsoleConfig(AppProps props) throws SQLException {
         if (props.isH2ConsoleEnabled()) {
             Console.main();
         }
