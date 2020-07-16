67704074ebe3a595e6cffc9ff13f8e8fb0d1a9d3
Sergey Kubatko <skubatko@gmail.com>
2020-07-16 21:29:09 +0300

    spring-13-hw / Changelog introduced

--- spring-13-hw/src/main/java/ru/skubatko/dev/otus/spring/hw13/App.java
+++ spring-13-hw/src/main/java/ru/skubatko/dev/otus/spring/hw13/App.java
@@ -5,8 +5,10 @@ package ru.skubatko.dev.otus.spring.hw13;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class App {
 
     public static void main(String[] args) {
---
+++ spring-13-hw/src/main/java/ru/skubatko/dev/otus/spring/hw13/config/AppConfig.java
@@ -0,0 +1,19 @@
+package ru.skubatko.dev.otus.spring.hw13.config;
+
+import com.github.cloudyrock.mongock.Mongock;
+import com.github.cloudyrock.mongock.SpringMongockBuilder;
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    private static final String CHANGELOGS_PACKAGE = "ru.skubatko.dev.otus.spring.hw13.changelogs";
+
+    @Bean
+    public Mongock mongock(MongoProps mongoProps, MongoClient mongoClient) {
+        return new SpringMongockBuilder(mongoClient, mongoProps.getDatabase(), CHANGELOGS_PACKAGE)
+                .build();
+    }
+}
