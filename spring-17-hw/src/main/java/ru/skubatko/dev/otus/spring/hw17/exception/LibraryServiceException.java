efade83d0684046eeb4d41bf41ba0b9bbe13905b
Sergey Kubatko <skubatko@gmail.com>
2020-08-03 17:15:22 +0300

    spring-17-hw / initial setup

---
+++ spring-17-hw/src/main/java/ru/skubatko/dev/otus/spring/hw17/exception/LibraryServiceException.java
@@ -0,0 +1,4 @@
+package ru.skubatko.dev.otus.spring.hw17.exception;
+
+public class LibraryServiceException extends RuntimeException {
+}
---
+++ spring-17-hw/src/main/java/ru/skubatko/dev/otus/spring/hw17/repository/BookRepository.java
@@ -0,0 +1,23 @@
+package ru.skubatko.dev.otus.spring.hw17.repository;
+
+import ru.skubatko.dev.otus.spring.hw17.domain.Author;
+import ru.skubatko.dev.otus.spring.hw17.domain.Book;
+import ru.skubatko.dev.otus.spring.hw17.domain.Genre;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @EntityGraph(attributePaths = {"author", "genre", "comments"})
+    @Override
+    List<Book> findAll();
+
+    Book findByName(String name);
+
+    List<Book> findByAuthor(Author author);
+
+    List<Book> findByGenre(Genre genre);
+}
