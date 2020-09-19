dcbad6c97f14d51534663e9c672e717be04838c7
Sergey Kubatko <skubatko@gmail.com>
2020-09-19 17:17:43 +0300

    spring-28-hw / complete

---
+++ spring-28-hw/src/main/java/ru/skubatko/dev/otus/spring/hw28/domain/Beauty.java
@@ -0,0 +1,11 @@
+package ru.skubatko.dev.otus.spring.hw28.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Beauty {
+
+    private String item;
+}
